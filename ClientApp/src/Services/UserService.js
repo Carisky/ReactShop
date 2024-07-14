2e5182702056c352243f2351c1f8d34a55dccace
Yehor <egorkorsun7@gmail.com>
2024-07-14 22:12:22 +0200

    add check for is expired token

--- ClientApp/src/Services/UserService.js
+++ ClientApp/src/Services/UserService.js
@@ -1,41 +1,51 @@
 class UserService {
-    static key = "user";
-  
-    static setUser(token) {
-      localStorage.setItem(this.key, token);
-    }
-  
-    static getUser() {
-      return localStorage.getItem(this.key);
-    }
-  
-    static clearUser() {
-      localStorage.removeItem(this.key);
-    }
-  
-    static isAdmin() {
-      // Example: Decode the JWT token to check roles
-      const token = localStorage.getItem(this.key);
-      if (!token) return false;
-  
-      // Decode token (example only, actual decoding depends on your JWT library)
-      const decodedToken = parseJwt(token);
-      return decodedToken.role === "admin";
-    }
+  static key = "user";
+
+  static setUser(token) {
+    localStorage.setItem(this.key, token);
   }
-  
-  function parseJwt(token) {
-    const base64Url = token.split(".")[1];
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split("")
-        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-        .join("")
-    );
-    return JSON.parse(jsonPayload);
+
+  static getUser() {
+    return localStorage.getItem(this.key);
+  }
+
+  static clearUser() {
+    localStorage.removeItem(this.key);
+  }
+
+  static isExpired() {
+    const token = localStorage.getItem(this.key);
+    const decodedToken = parseJwt(token);
+    const currentTime = Math.floor(Date.now() / 1000);
+
+
+    if (decodedToken.exp > currentTime) {
+      return false;
+    }else return true
+  }
+
+  static isAdmin() {
+    const token = localStorage.getItem(this.key);
+    if (!token) return false;
+    if(!this.isExpired){
+      return false
+    }
     
+    const decodedToken = parseJwt(token);
+    return decodedToken.role === "admin";
   }
-  
-  export default UserService;
-  
+}
+
+function parseJwt(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .join("")
+  );
+  return JSON.parse(jsonPayload);
+}
+
+export default UserService;
