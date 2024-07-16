4c724f5a198827c5ea7673f5083b6af0b395a0ac
Yehor <egorkorsun7@gmail.com>
2024-07-16 14:34:56 +0200

    improve logic

--- ClientApp/src/Services/RecomendedProducts.js
+++ ClientApp/src/Services/RecomendedProducts.js
@@ -1,46 +1,50 @@
 class RecomendedProducts {
-    static key = "RecomendedProductsTags";
+  static key = "RecomendedProductsTags";
+  static limit = 10;
 
-    // Get the tags from localStorage
-    static get() {
-        const tags = localStorage.getItem(this.key);
-        if (tags) {
-            return JSON.parse(tags);
-        } else {
-            const initialTags = {};
-            localStorage.setItem(this.key, JSON.stringify(initialTags));
-            return initialTags;
-        }
+  
+  static get() {
+    const tags = localStorage.getItem(this.key);
+    if (tags) {
+      return JSON.parse(tags);
+    } else {
+      const initialTags = {};
+      localStorage.setItem(this.key, JSON.stringify(initialTags));
+      return initialTags;
     }
+  }
 
-    // Increment the count for a specific tag
-    static increment(tag) {
-        const tags = this.get();
-        if (tags[tag]) {
-            tags[tag]++;
-        } else {
-            tags[tag] = 1;
-        }
-        localStorage.setItem(this.key, JSON.stringify(tags));
+  
+  static increment(tag) {
+    const tags = this.get();
+    if (tags[tag]) {
+      if (tags[tag] != this.limit) {
+        tags[tag]++;
+      }
+    } else {
+      tags[tag] = 1;
     }
+    localStorage.setItem(this.key, JSON.stringify(tags));
+  }
 
-    // Reset the tags to an empty object
-    static reset() {
-        const initialTags = {};
-        localStorage.setItem(this.key, JSON.stringify(initialTags));
+  
+  static reset() {
+    const initialTags = {};
+    localStorage.setItem(this.key, JSON.stringify(initialTags));
+  }
+
+  static getMostVisited(limit = null) {
+    const tags = this.get();
+    const sortedTags = Object.entries(tags).sort(
+      ([, countA], [, countB]) => countB - countA
+    );
+
+    if (limit !== null) {
+      return sortedTags.slice(0, limit).map(([tag, count]) => ({ tag, count }));
+    } else {
+      return sortedTags.map(([tag, count]) => ({ tag, count }));
     }
-
-    static getMostVisited(limit = null) {
-        const tags = this.get();
-        const sortedTags = Object.entries(tags)
-            .sort(([, countA], [, countB]) => countB - countA);
-
-        if (limit !== null) {
-            return sortedTags.slice(0, limit).map(([tag, count]) => ({ tag, count }));
-        } else {
-            return sortedTags.map(([tag, count]) => ({ tag, count }));
-        }
-    }
+  }
 }
 
 export default RecomendedProducts;
