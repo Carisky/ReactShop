1c9367746b5e1922eda9ba8ded1da5a417be3ad2
Yehor <egorkorsun7@gmail.com>
2024-07-16 14:34:38 +0200

    add routes

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    ['/products','/recommendedproducts','/auth/login',"/admin/articles","/ProductImages","/admin/validation/"],
+    ['/products','/recommendedproducts','/auth/login',"/admin/articles","/ProductImages","/admin/validation/","products/ids"],
     createProxyMiddleware({
       target: 'https://localhost:7200', 
       changeOrigin: true,
