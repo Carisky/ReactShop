76bed1b61c376cecb0748061c0d287156b58504c
Yehor <egorkorsun7@gmail.com>
2024-07-18 17:37:19 +0200

    Add email handling to db model

---
+++ Migrations/20240718150223_AddEmailToOrders.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReactShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailToOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Orders");
+        }
+    }
+}
