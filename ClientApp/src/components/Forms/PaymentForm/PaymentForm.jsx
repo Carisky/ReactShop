5d436629b0c6a3cd1c09e8be187b1605bf99cf19
Yehor <egorkorsun7@gmail.com>
2024-07-20 09:41:41 +0200

    use redux instead of hande wroted services

--- ClientApp/src/components/Forms/PaymentForm/PaymentForm.jsx
+++ ClientApp/src/components/Forms/PaymentForm/PaymentForm.jsx
@@ -1,59 +1,49 @@
-import React, { useEffect, useRef } from 'react';
+// src/Forms/PaymentForm/PaymentForm.js
 import axios from 'axios';
-import CartService from '../../../Services/CartService';
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { checkStock, createPayPalButtons } from '../../../utils/paymentUtils';
+import { clearCart } from '../../../redux/cartSlice';
 
 const PaymentForm = () => {
   const paypalRef = useRef();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.items);
   useEffect(() => {
-    const cartItems = CartService.getCart();
+    const initializePayPal = async () => {
+      try {
+        const { totalPrice } = await checkStock(cartItems);
 
-    axios.post('/Order/CheckStock', cartItems)
-      .then(response => {
-        const { totalPrice } = response.data;
-        window.paypal.Buttons({
-          createOrder: (data, actions) => {
-            return actions.order.create({
-              purchase_units: [{
-                amount: {
-                  value: totalPrice.toString() 
-                }
-              }]
-            });
-          },
-          onApprove: (data, actions) => {
-            return actions.order.capture().then(details => {
-              const fullname = details.payer.name.given_name + ' ' + details.payer.name.surname;
-              const address = details.purchase_units[0].shipping.address;
-              const email = details.payer.email_address;
-              const orderData = {
-                fullname,
-                email,
-                address: `${address.address_line_1}, ${address.admin_area_2}, ${address.admin_area_1}, ${address.postal_code}, ${address.country_code}`,
-                items: cartItems
-              };
+        const handleOrderCreation = async (details) => {
+          const fullname = details.payer.name.given_name + ' ' + details.payer.name.surname;
+          const address = details.purchase_units[0].shipping.address;
+          const email = details.payer.email_address;
+          const orderData = {
+            fullname,
+            email,
+            address: `${address.address_line_1}, ${address.admin_area_2}, ${address.admin_area_1}, ${address.postal_code}, ${address.country_code}`,
+            items: cartItems,
+          };
 
-              axios.post('/Order/CreateOrder', orderData)
-                .then(response => {
-                  alert('Order successfully created and transaction completed by ' + details.payer.name.given_name);
-                  CartService.clearCart();
-                  
-                })
-                .catch(error => {
-                  console.error('Error creating order:', error);
-                });
-            });
-          },
-          onError: (err) => {
-            console.error(err);
+          try {
+            await axios.post('/Order/CreateOrder', orderData);
+            dispatch(clearCart());
+
+          } catch (error) {
+            console.error('Error creating order:', error);
           }
-        }).render(paypalRef.current);
-      })
-      .catch(error => {
-        console.error('Error checking stock or calculating total price:', error);
-      });
-  }, []);
+        };
+
+        createPayPalButtons(paypalRef, totalPrice, cartItems, handleOrderCreation);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    initializePayPal();
+  }, [cartItems, dispatch, navigate]);
 
   return (
     <div>
