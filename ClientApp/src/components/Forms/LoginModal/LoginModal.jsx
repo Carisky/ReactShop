6dea0eabc8eb9e142330fda22c23574505dceab8
Yehor <egorkorsun7@gmail.com>
2024-08-12 18:47:31 +0200

    implement login logic

--- ClientApp/src/components/Forms/LoginModal/LoginModal.jsx
+++ ClientApp/src/components/Forms/LoginModal/LoginModal.jsx
@@ -1,17 +1,72 @@
 import React from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from '@mui/material';
+import { useForm, Controller } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../../redux/userSlice";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginModal = ({ open, handleClose }) => {
+
+    const dispatch = useDispatch();
+    const { control, handleSubmit, formState: { errors } } = useForm();
+
+
+    const handleFormSubmit = async (formData) => {
+        try {
+          const response = await axios.post("/auth/login", formData);
+          const { token } = response.data;
+          dispatch(setUser(token));
+          handleClose();
+        } catch (error) {
+          console.error("Login error:", error);
+        }
+      };
+
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>Login</DialogTitle>
       <DialogContent>
-        <TextField label="Username" fullWidth margin="dense" />
-        <TextField label="Password" type="password" fullWidth margin="dense" />
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+          <Controller
+            name="username"
+            control={control}
+            defaultValue=""
+            rules={{ required: "Username is required" }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Username"
+                variant="outlined"
+                error={!!errors.username}
+                helperText={errors.username ? errors.username.message : ""}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            rules={{ required: "Password is required" }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Password"
+                type="password"
+                variant="outlined"
+                error={!!errors.password}
+                helperText={errors.password ? errors.password.message : ""}
+              />
+            )}
+          />
+
+        </div>
+      </form>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">Cancel</Button>
-        <Button onClick={handleClose} color="primary">Login</Button>
+        <Button onClick={handleSubmit(handleFormSubmit)} color="primary">Login</Button>
       </DialogActions>
     </Dialog>
   );
